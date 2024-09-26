accdcadbe07366acadf0db72a6793aa5304df1bc
Bas de Reus <dereusbas461@gmail.com>
2024-09-26 02:24:06 +0200

    Added movement and crowd

---
+++ Assets/Scripts/Environment/Gun.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Environment
+{
+    public sealed class Gun : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+using Framework.Extensions;
+
+namespace Player
+{
+    public sealed class Movement : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D rigidbody2D;
+        [SerializeField] private float speed = 3;
+
+        private void Awake()
+        {
+            if (!rigidbody2D)
+                rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            float input = Input.GetAxisRaw("Horizontal");
+
+            if (input != 0)
+            {
+                Vector2 targetScale = Vector2.one;
+                targetScale.x = input;
+                transform.localScale = targetScale;
+            }
+            
+            float yesSpeed = speed * input;
+            
+            Vector2 targetVelocity = rigidbody2D.velocity;
+            targetVelocity.SetX(yesSpeed);
+            rigidbody2D.velocity = targetVelocity;
+        }
+    }
+}
