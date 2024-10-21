86b5fbbfe5fded452d5b0da39f89b69343431f4d
João Jubitana <70643013+Kazuma-47@users.noreply.github.com>
2024-10-21 14:09:44 +0200

    applied code review changes

--- Assets/Scripts/NPC/NpcConfig.cs
+++ Assets/Scripts/NPC/NpcConfig.cs
@@ -1,34 +1,42 @@
 using UnityEngine;
 
-using static NpcSystem.NpcTypes;
-
 namespace NpcSystem
 {
     sealed class NpcConfig : MonoBehaviour
     {
-        [SerializeField , Tooltip("prefab for the weapon")] private GameObject WeaponPrefab;
-        [SerializeField, Tooltip("position for the weapon to spawn")] private Transform WeaponSpawnLocation;
+        [SerializeField] private GameObject WeaponPrefab;
+        [SerializeField] private Transform WeaponSpawnLocation;
         [SerializeField , Tooltip("prefabs of the implemented animated characters")] private GameObject[] characters; 
         [SerializeField , Tooltip("location for the animated character to spanw(NOT world position)")] private Transform charactersSpawn;
-        
+
+        private void Awake()
+        {
+            if (characters.Length == 0)
+            {
+                Debug.LogWarning("skins not yet implemented");
+            }
+        }
+
         //<summary>
         //set everything before the character spawns
         //</summary>
         //<param name ="type"> will pass what kind of character will be spawned</param>
         public void SetNpc(NpcType type)
         {
-            Debug.LogWarning("skins not yet implemented");
-            if (type == NpcType.Armed) 
+            switch (type)
             {
-                Instantiate(WeaponPrefab, WeaponSpawnLocation.position, Quaternion.identity, WeaponSpawnLocation);
-            }
+                case NpcType.Armed:
+                {
+                    Instantiate(WeaponPrefab, WeaponSpawnLocation.position, Quaternion.identity, WeaponSpawnLocation);
+                    break;
+                }       
+            }   
         }
         
         private void SetRandomSkin()
         {
             int randomIndex = Random.Range(0, characters.Length);
-            Instantiate(characters[randomIndex], charactersSpawn.position , Quaternion.identity);
+            Instantiate(characters[randomIndex], charactersSpawn.position , Quaternion.identity, gameObject.transform);
         }
-
     }
 }
