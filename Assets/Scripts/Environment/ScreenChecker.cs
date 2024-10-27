5c406d74638fc807417172986f0ecccada1974e5
Bas de Reus <dereusbas461@gmail.com>
2024-10-27 18:27:18 +0100

    Added weapon timer UI

---
+++ Assets/Scripts/Environment/ScreenChecker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Environment
+{
+    public sealed class ScreenChecker : MonoBehaviour
+    {
+        [SerializeField] private List<TargetScreenInfo> screenPositions; 
+        
+        private Camera mainCamera;
+
+        private void Start()
+        {
+            mainCamera = Camera.main;
+
+            if (mainCamera == null)
+                Debug.LogError("Main camera not found.");
+        }
+
+        private void Update()
+        {
+            CheckPositions();
+
+            foreach (TargetScreenInfo screenInfo in screenPositions) 
+                Debug.Log(screenInfo.position + " " + screenInfo.target.name);
+        }
+
+        private void CheckPositions()
+        {
+            for (int i = 0; i < screenPositions.Count; i++)
+            {
+                TargetScreenInfo targetScreenInfo = screenPositions[i];
+                Vector3 screenPoint = mainCamera.WorldToScreenPoint(targetScreenInfo.target.position);
+
+                ScreenPosition position = screenPoint.x switch
+                {
+                    >= 0 when screenPoint.x <= Screen.width 
+                              && screenPoint.y >= 0 
+                              && screenPoint.y <= Screen.height =>
+                        ScreenPosition.INSIDE,
+                    < 0 => ScreenPosition.OUTSIDE_LEFT,
+                    _ => screenPoint.x > Screen.width
+                        ? ScreenPosition.OUTSIDE_RIGHT
+                        : ScreenPosition.INSIDE
+                };
+
+                targetScreenInfo.position = position;
+                screenPositions[i] = targetScreenInfo;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/World/WeaponTimerUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using Framework;
+
+namespace UI.World
+{
+    public sealed class WeaponTimerUI : MonoBehaviour
+    {
+        [SerializeField] private Timer timer;
+        [SerializeField] private Image background;
+        [SerializeField] private Gradient gradient;
+
+        private bool _shouldUpdate;
+        
+        private void Update()
+        {
+            if (!_shouldUpdate)
+                return;
+            
+            float t = 1 - timer.GetCurrentTimerPercentage();
+            background.fillAmount = t;
+            background.color = gradient.Evaluate(t);
+        }
+
+        private void OnEnable() => _shouldUpdate = true;
+
+        private void OnDisable() => _shouldUpdate = false;
+    }
+}
