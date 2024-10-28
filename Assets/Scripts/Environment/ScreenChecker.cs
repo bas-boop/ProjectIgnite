ee8b49ff82b73ca14a9c90bbdc0e3c00dacbe824
Bas de Reus <dereusbas461@gmail.com>
2024-10-28 16:32:11 +0100

    Added working side iducators

--- Assets/Scripts/Environment/ScreenChecker.cs
+++ Assets/Scripts/Environment/ScreenChecker.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
+using UI.World;
 
 namespace Environment
 {
     public sealed class ScreenChecker : MonoBehaviour
     {
-        [SerializeField] private List<TargetScreenInfo> screenPositions; 
+        [SerializeField] private float margin = 0.5f;
+        [SerializeField] private GameObject leftUI;
+        [SerializeField] private GameObject rightUI;
+        [SerializeField] private List<TargetScreenInfo> screenPositions;
         
         private Camera mainCamera;
 
@@ -21,33 +27,78 @@ namespace Environment
         private void Update()
         {
             CheckPositions();
-
-            foreach (TargetScreenInfo screenInfo in screenPositions) 
-                Debug.Log(screenInfo.position + " " + screenInfo.target.name);
+            UpdateUI();
         }
 
         private void CheckPositions()
         {
+            float leftBound = 0 + margin;
+            float rightBound = Screen.width - margin;
+
             for (int i = 0; i < screenPositions.Count; i++)
             {
                 TargetScreenInfo targetScreenInfo = screenPositions[i];
                 Vector3 screenPoint = mainCamera.WorldToScreenPoint(targetScreenInfo.target.position);
 
-                ScreenPosition position = screenPoint.x switch
-                {
-                    >= 0 when screenPoint.x <= Screen.width 
-                              && screenPoint.y >= 0 
-                              && screenPoint.y <= Screen.height =>
-                        ScreenPosition.INSIDE,
-                    < 0 => ScreenPosition.OUTSIDE_LEFT,
-                    _ => screenPoint.x > Screen.width
-                        ? ScreenPosition.OUTSIDE_RIGHT
-                        : ScreenPosition.INSIDE
-                };
+                ScreenPosition position;
+
+                if (screenPoint.x >= leftBound
+                    && screenPoint.x <= rightBound
+                    && screenPoint.y >= 0
+                    && screenPoint.y <= Screen.height)
+                    position = ScreenPosition.INSIDE;
+                else if (screenPoint.x < leftBound)
+                    position = ScreenPosition.OUTSIDE_LEFT;
+                else if (screenPoint.x > rightBound)
+                    position = ScreenPosition.OUTSIDE_RIGHT;
+                else
+                    position = ScreenPosition.INSIDE;
 
                 targetScreenInfo.position = position;
                 screenPositions[i] = targetScreenInfo;
             }
+        }
+
+        private void UpdateUI()
+        {
+            bool l = false;
+            bool r = false;
+            
+            foreach (TargetScreenInfo screenInfo in screenPositions)
+            {
+                switch (screenInfo.position)
+                {
+                    case ScreenPosition.INSIDE:
+                        break;
+                    case ScreenPosition.OUTSIDE_LEFT:
+                        l = true;
+                        break;
+                    case ScreenPosition.OUTSIDE_RIGHT:
+                        r = true;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            leftUI.SetActive(l);
+            rightUI.SetActive(r);
+
+            ApplyProgress(l, leftUI, ScreenPosition.OUTSIDE_LEFT);
+            ApplyProgress(r, rightUI, ScreenPosition.OUTSIDE_RIGHT);
+        }
+
+        private void ApplyProgress(bool side,
+            GameObject ui,
+            ScreenPosition yes)
+        {
+            if (!side)
+                return;
+            
+            float highestProgress = (from screenInfo in screenPositions where screenInfo.position == yes 
+                select screenInfo.target.GetComponent<WeaponTimerUI>().Progress).Prepend(0).Max();
+
+            ui.GetComponent<WeaponTimerUI>().Progress = highestProgress;
         }
     }
 }
--- Assets/Scripts/UI/World/WeaponTimerUI.cs
+++ Assets/Scripts/UI/World/WeaponTimerUI.cs
@@ -10,6 +10,7 @@ namespace UI.World
         [SerializeField] private Timer timer;
         [SerializeField] private Image background;
         [SerializeField] private Gradient gradient;
+        public float Progress { get; set; }
 
         private bool _shouldUpdate;
         
@@ -18,13 +19,17 @@ namespace UI.World
             if (!_shouldUpdate)
                 return;
             
-            float t = 1 - timer.GetCurrentTimerPercentage();
-            background.fillAmount = t;
-            background.color = gradient.Evaluate(t);
+            if (timer)
+                Progress = 1 - timer.GetCurrentTimerPercentage();
+            
+            background.fillAmount = Progress;
+            background.color = gradient.Evaluate(Progress);
         }
 
         private void OnEnable() => _shouldUpdate = true;
 
         private void OnDisable() => _shouldUpdate = false;
+
+        public void SetTimer(Timer target) => timer = target;
     }
 }
