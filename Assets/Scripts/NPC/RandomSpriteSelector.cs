e7899a39db474b1868b0eb31d864a50c83a4ee5e
Bas de Reus <dereusbas461@gmail.com>
2024-11-01 15:53:45 +0100

    Randmize the crowd

---
+++ Assets/Scripts/NPC/RandomSpriteSelector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace NPC
+{
+    public sealed class RandomSpriteSelector : MonoBehaviour
+    {
+        [SerializeField] public Sprite[] sprites;
+
+        private SpriteRenderer _spriteRenderer;
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        
+            if (sprites.Length > 0)
+                SetRandomMaterial();
+            else
+                Debug.LogError("No materials found in the materials array. Please assign some materials.");
+        }
+
+        private void SetRandomMaterial()
+        {
+            int randomNumber = Random.Range(0, sprites.Length);
+            _spriteRenderer.sprite = sprites[randomNumber];
+        }
+    }
+}
